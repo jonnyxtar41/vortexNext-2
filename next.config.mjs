f11997b143ccc7de94e016f2b665febf2582c146
Jonny <jonny@gmail.com>
2025-11-11 00:33:16 -0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-import dotenv from 'dotenv';
-
-dotenv.config({ path: '.env.local' });
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-  },
+  // No necesitas NADA MÁS.
+  // Next.js maneja .env.local y NEXT_PUBLIC_ automáticamente.
 };
 
 export default nextConfig;
