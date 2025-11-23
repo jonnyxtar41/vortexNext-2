7f63b7c933be27496c47315a6685fd68adab1883
Jonny <jonny@gmail.com>
2025-11-23 06:35:44 -0500

    update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,15 @@
 import './globals.css';
 import { Providers } from './providers';
+import Script from 'next/script';
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es" suppressHydrationWarning={true}>
+      <Script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5631795975075125"
+            crossOrigin="anonymous"
+        />
       <body>
         <Providers>
           {children}
