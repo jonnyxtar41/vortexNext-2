0b806e1be147cd33a7e97dce3951ceb19053bddc
Jonny <jonny@gmail.com>
2025-11-30 09:00:45 -0500

    update

--- app/utils/supabase/client.js
+++ app/utils/supabase/client.js
@@ -2,9 +2,13 @@
 import { createBrowserClient } from '@supabase/ssr';
 
 export function createClient() {
+    // --- AGREGA ESTO ---
+  console.log("--- DEBUGGING SUPABASE ---");
+  console.log("URL leída:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("KEY leída:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY.substring(0, 10) + "..." : "UNDEFINED");
 
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
   );
-}
+} 
